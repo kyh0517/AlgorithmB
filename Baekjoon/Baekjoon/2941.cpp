a4acc7a74fb05d3c5b72c61118dc058359811df8
kyh0517 <sunbong9999@gmail.com>
2019-03-19 17:44:03 +0900

    2941

--- Baekjoon/Baekjoon/2941.cpp
+++ Baekjoon/Baekjoon/2941.cpp
@@ -1,10 +1,101 @@
 #include<iostream>
-
+#include<string>
 using namespace std;
 
 int main() {
 
+	string input;
+	int size, i, count = 0;
+	
+	cin >> input;
+	size = input.size();
+	for (i = 0; i < size;) {
+		if (i < size) {
+			if (input.at(i) == 'c' && i+1 < size) {
+				if (input.at(i+1) == '=') {
+					i += 2;
+					count++;
+				}
+				else if (input.at(i+1) == '-') {
+					i += 2;
+					count++;
+				}
+				else {
+					i++;
+					count++;
+				}
+			}
+			else if (input.at(i) == 'd' && i + 1 < size) {
+				
+				if (input.at(i + 1) == 'z') {
+					if (i + 2 < size && input.at(i + 2) == '=') {
+						i += 3;
+						count++;
+					}
+					else {
 
+						i+=2;
+						count+=2;
+					}
+				}
+				else if (input.at(i + 1) == '-') {
+					i += 2;
+					count++;
+				}
+				else {
+					i++;
+					count++;
+				}
+			}
+			else if (input.at(i) == 'l' && i + 1 < size) {
+				if (input.at(i + 1) == 'j') {
+					i += 2;
+					count++;
+				}
+				else {
+					i++;
+					count++;
+				}
+			}
+			else if (input.at(i) == 'n' && i + 1 < size) {
+				if (input.at(i + 1) == 'j') {
+					i += 2;
+					count++;
+				}
+				else {
+					i++;
+					count++;
+				}
+			}
+			else if (input.at(i) == 's' && i + 1 < size) {
+				if (input.at(i + 1) == '=') {
+					i += 2;
+					count++;
+				}
+				else {
+					i++;
+					count++;
+				}
+			}
+			else if (input.at(i) == 'z' && i + 1 < size) {
+				if (input.at(i + 1) == '=') {
+					i += 2;
+					count++;
+				}
+				else {
+					i++;
+					count++;
+				}
+			}
+			else {
+				i++;
+				count++;
+			}
+		}
+		
+	}
+
+	cout << count;
 
 	return 0;
 }
