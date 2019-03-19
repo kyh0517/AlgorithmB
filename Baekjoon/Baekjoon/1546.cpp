ab6a4d781a104b8552d5a9717a7d36f5348ff31d
kyh0517 <sunbong9999@gmail.com>
2019-03-19 17:00:49 +0900

    1546

---
+++ Baekjoon/Baekjoon/1546.cpp
@@ -0,0 +1,33 @@
+/*
+#include<iostream>
+#include<algorithm>
+
+using namespace std;
+
+int main() {
+
+	int input;
+	double maxScore;
+	double aver = 0;
+	int i;
+	cin >> input;
+
+	double *score;
+
+	score = (double *)malloc(sizeof(double)*input);
+	
+	for (i = 0; i < input; i++) {
+		cin >> score[i];
+	}
+
+	maxScore = *max_element(score, score + input);
+
+	for (i = 0; i < input; i++) {
+		aver = aver + (double)(score[i] / maxScore * 100);
+	}
+
+	cout << aver / input;
+
+	return 0;
+}
+*/
